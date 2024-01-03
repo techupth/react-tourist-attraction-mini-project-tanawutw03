725b42410ea64af87ab16de1f5a6b78a3b85ba5f
Tanawut Wongboot <tanawut.w@outlook.com>
2024-01-03 22:20:00 +0700

    fix: HRM

--- README.md
+++
@@ -1,1 +0,0 @@
-## react-tourist-attraction-mini-project
--- client/src/components/SearchBox.jsx
+++ client/src/components/SearchBox.jsx
@@ -13,19 +13,18 @@ function SearchBox() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex items-center">
-      <fieldset>
+    <form onSubmit={handleSubmit} className="flex flex-col">
+      <label htmlFor="search-input" className="">
         ค้นหาที่เที่ยว
-        <label htmlFor="search-input"></label>
-        <input
-          type="text"
-          id="search-input"
-          value={searchTerm}
-          onChange={handleChange}
-          placeholder="หาที่เที่ยวแล้วไปกัน ..."
-          className="bg-gray-200 p-4 rounded-lg w-full mr-4 focus:outline-none focus:ring-2 focus:ring-blue-500 text-center"
-        />
-      </fieldset>
+      </label>
+      <input
+        type="text"
+        id="search-input"
+        value={searchTerm}
+        onChange={handleChange}
+        placeholder="หาที่เที่ยวแล้วไปกัน ..."
+        className="bg-gray-200 p-4 rounded-lg w-full mr-4 focus:outline-none focus:ring-2 focus:ring-blue-500 text-center mt-5"
+      />
     </form>
   );
 }
