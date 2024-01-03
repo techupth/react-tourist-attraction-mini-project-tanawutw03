3f4cdd0bb7d4bbef4b7931b2e824a2806c75e19a
Tanawut Wongboot <tanawut.w@outlook.com>
2024-01-03 21:16:02 +0700

    feat: add tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+    <div className="App">
+      {/* Start coding here */}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
